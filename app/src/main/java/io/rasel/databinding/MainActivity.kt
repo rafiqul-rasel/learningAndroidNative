3818b10abd6bd79be1b9cfbf04deb8fe6c626826
Md.Rafiqul Islam <rafiqul.islam@brainstation-23.com>
2023-07-04 01:36:09 +0600

    add a button with  ProgressBar for implementing view binding and data binding

--- app/src/main/java/io/rasel/databinding/MainActivity.kt
+++ app/src/main/java/io/rasel/databinding/MainActivity.kt
@@ -2,10 +2,28 @@ package io.rasel.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import io.rasel.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Initialize View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.text = "Show"
+        binding.progressBar.visibility= View.GONE
+        binding.button.setOnClickListener {
+           if(binding.progressBar.visibility== View.GONE) {
+               binding.button.text = "Hide"
+               binding.progressBar.visibility= View.VISIBLE
+           }else{
+               binding.button.text = "Show"
+               binding.progressBar.visibility= View.GONE
+           }
+        }
     }
 }
